59cf74ba37c695926d1f803f25b4c45aad29550f
lu <46019075+lugf027@users.noreply.github.com>
2022-10-11 23:46:16 +0800

    java grpc & spring init project

---
+++ spring_init/src/test/java/com/lugf027/spring_init/SpringInitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lugf027.spring_init;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringInitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
